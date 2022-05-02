6c9b537d16273ecdfefa95815306d86334940d87
Aslan <a.korpeyev@alabs.team>
2022-05-03 00:32:08 +0600

    finish step 11

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ import io from 'socket.io-client';
 import { store } from 'store/index';
 import SocketProvider from 'context/SocketContext';
 import { Provider as RollBarProvider, ErrorBoundary, LEVEL_INFO } from '@rollbar/react';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
 import App from './App';
+import resources from './locales/ru.json';
 
 const socket = io();
 if (process.env.NODE_ENV !== 'production') {
@@ -28,18 +31,28 @@ const rollbarConfig = {
   },
 };
 
+i18next.use(initReactI18next).init({
+  resources,
+  fallbackLng: 'ru',
+  debug: process.env.NODE_ENV === 'development',
+  lng: 'ru',
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
 const container = document.getElementById('chat');
 const root = createRoot(container);
 
 // eslint-disable-next-line react/jsx-filename-extension
 root.render(
-  <BrowserRouter>
-    <RollBarProvider config={rollbarConfig}>
-      <ErrorBoundary level={LEVEL_INFO}>
+  <RollBarProvider config={rollbarConfig}>
+    <ErrorBoundary level={LEVEL_INFO}>
+      <BrowserRouter>
         <Provider store={store}>
           <SocketProvider socket={socket}><App /></SocketProvider>
         </Provider>
-      </ErrorBoundary>
-    </RollBarProvider>
-  </BrowserRouter>,
+      </BrowserRouter>
+    </ErrorBoundary>
+  </RollBarProvider>,
 );
